9853ae717da89bf252531ea70e2c02c464bcdd3c
Hanchung Lee <lee.hanchung@gmail.com>
2023-11-26 00:25:40 +0000

    feat: fix model

--- app/app.py
+++ app/app.py
@@ -109,7 +109,7 @@ async def start():
         raise SystemError
 
     llm = ChatOpenAI(
-        model='gpt-3.5-turbo-1106',
+        model='gpt-3.5-turbo-16k-0613',
         temperature=0,
         streaming=True
     )
