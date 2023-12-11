88dbf75f0e3baf5bb1a6a5bad1c2552cb99fefb6
Han <lee.hanchung@gmail.com>
2023-12-11 00:25:24 +0000

    fix: prompt regression

--- app/prompt.py
+++ app/prompt.py
@@ -8,7 +8,8 @@ To get started:
 2. Ask any question about the file!
 """
 
-template = """Given the following extracted parts of a long document and a question, create a final answer with references ("SOURCES"). 
+template = """Please act as an expert financial analyst when you answer the questions and pay special attention to the financial statements.  Operating margin is also known as op margin and is calculated by dividing operating income by revenue.
+Given the following extracted parts of a long document and a question, create a final answer with references ("SOURCES").
 If you don't know the answer, just say that you don't know. Don't try to make up an answer.
 ALWAYS return a "SOURCES" field in your answer, with the format "SOURCES: <source1>, <source2>, <source3>, ...".
 
