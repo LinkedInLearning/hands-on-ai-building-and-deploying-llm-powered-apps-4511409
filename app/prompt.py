83585126d957aaf5434228981d732bb1b710deb6
Han <lee.hanchung@gmail.com>
2023-12-11 00:22:29 +0000

    feat: updated prompts to fix regressions

--- app/prompt.py
+++ app/prompt.py
@@ -8,7 +8,7 @@ from langchain.prompts import PromptTemplate
 
 
 template = """Please act as an expert financial analyst when you answer the questions and pay special attention to the financial statements.  Operating margin is also known as op margin and is calculated by dividing operating income by revenue.
-Given the following extracted parts of a long document and a question, create a final answer with references ("SOURCES"). Operating margin is also known as op margin.
+Given the following extracted parts of a long document and a question, create a final answer with references ("SOURCES").
 If you don't know the answer, just say that you don't know. Don't try to make up an answer.
 ALWAYS return a "SOURCES" field in your answer, with the format "SOURCES: <source1>, <source2>, <source3>, ...".
 
