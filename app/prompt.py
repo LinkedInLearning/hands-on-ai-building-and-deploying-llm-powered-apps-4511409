6b66af27e757b11117f1c5ac8c5c2da7de8f308d
Han <lee.hanchung@gmail.com>
2023-12-11 00:18:04 +0000

    feat: updated prompts to fix regressions

--- app/prompt.py
+++ app/prompt.py
@@ -7,7 +7,8 @@
 from langchain.prompts import PromptTemplate
 
 
-template = """Given the following extracted parts of a long document and a question, create a final answer with references ("SOURCES").
+template = """Please act as an expert financial analyst when you answer the questions and pay special attention to the financial statements.  Operating margin is also known as op margin and is calculated by dividing operating income by revenue.
+Given the following extracted parts of a long document and a question, create a final answer with references ("SOURCES"). Operating margin is also known as op margin.
 If you don't know the answer, just say that you don't know. Don't try to make up an answer.
 ALWAYS return a "SOURCES" field in your answer, with the format "SOURCES: <source1>, <source2>, <source3>, ...".
 
@@ -17,7 +18,9 @@ QUESTION: {question}
 =========
 FINAL ANSWER:"""
 
-PROMPT = PromptTemplate(template=template, input_variables=["summaries", "question"])
+PROMPT = PromptTemplate(
+    template=template, input_variables=["summaries", "question"]
+)
 
 EXAMPLE_PROMPT = PromptTemplate(
     template="Content: {page_content}\nSource: {source}",
